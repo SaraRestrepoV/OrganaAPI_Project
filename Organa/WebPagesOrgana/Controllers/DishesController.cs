d8a0627b9cab6199212955c91c77ab771e5c6e8b
Sara Restrepo Vélez <89092194+SaraRestrepoV@users.noreply.github.com>
2023-05-23 22:12:42 -0500

    Web pages created with index of pages and dishes

---
+++ Organa/WebPagesOrgana/Controllers/DishesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Organa.DAL.Entities;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace WebPagesOrgana.Controllers
+{
+    public class DishesController : Controller
+    {
+        private readonly IHttpClientFactory _httpClient;
+
+        public DishesController(IHttpClientFactory httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+                var url = "https://localhost:7187/api/Dish/Get";
+                var json = await _httpClient.CreateClient().GetStringAsync(url);
+                List<Dish> Dishes = JsonConvert.DeserializeObject<List<Dish>>(json);
+                return View(Dishes);
+            } catch (Exception ex)
+            {
+                return View("Error", ex);
+            }          
+        }
+    }
+}
