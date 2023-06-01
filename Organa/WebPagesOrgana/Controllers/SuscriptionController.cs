2da0293cd551a95362aa4a9790c3ca59c58bf4b2
Maria Jose <majoyepesdiaz23@gmail.com>
2023-05-31 23:34:41 -0500

    Suscription View and linking

---
+++ Organa/WebPagesOrgana/Controllers/SuscriptionController.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Organa.DAL.Entities;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+using System.Security.Policy;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Net.Sockets;
+
+namespace WebPagesOrgana.Controllers
+{
+    public class SuscriptionController : Controller
+    {
+        private readonly IHttpClientFactory _httpClient;
+
+        public SuscriptionController(IHttpClientFactory httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<IActionResult> Index()
+        {
+           /* try
+            {
+                var url = "https://localhost:7187/api/Dish/Get";
+                var json = await _httpClient.CreateClient().GetStringAsync(url);
+                List<Dish> Dishes = JsonConvert.DeserializeObject<List<Dish>>(json);
+                return View(Dishes);
+            } catch (Exception ex)
+            {
+                return View("Error", ex);
+            }    */
+           
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Dish dishes)
+        {
+            try
+            {
+                var url = "https://localhost:7187/api/Dish/CreateDish";
+                await _httpClient.CreateClient().PostAsJsonAsync(url, dishes);
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                return View("Error", ex);
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            try
+            {
+                var url = String.Format("https://localhost:7187/api/Dish/Get/{0}", id);
+                var json = await _httpClient.CreateClient().GetStringAsync(url);
+                Dish dishes = JsonConvert.DeserializeObject<Dish>(json);
+                return View(dishes);
+            }
+            catch (Exception ex)
+            {
+                return View("Error", ex);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid? id, Dish  dishes)
+        {
+            try
+            {
+                var url = String.Format("https://localhost:7187/api/Dish/Edit/{0}", id);
+                await _httpClient.CreateClient().PutAsJsonAsync(url, dishes);
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                return View("Error", ex);
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            try
+            {
+                var url = String.Format("https://localhost:7187/api/Dish/Get/{0}", id);
+                var json = await _httpClient.CreateClient().GetStringAsync(url);
+                Dish dishes = JsonConvert.DeserializeObject<Dish>(json);
+                return View(dishes);
+            }
+            catch (Exception ex)
+            {
+                return View("Error", ex);
+            }
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            try
+            {
+                var url = String.Format("https://localhost:7187/api/Dish/Delete/{0}", id);
+                await _httpClient.CreateClient().DeleteAsync(url);
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                return View("Error", ex);
+            }
+        }
+    }
+}
